0210ecd203fd28c351f064d9d3147950ada40183
aleksandr khlapov <alxlapov@outlook.com>
2020-10-16 01:08:34 +0300

    f: seo test

--- tests/seo/SeoCest.php
+++ tests/seo/SeoCest.php
@@ -9,21 +9,19 @@ class SeoCest {
      * @param Example $example
      */
     public function csv(SeoTester $i, Example $example) {
-        $expected = array(
-            'url'=>$example['url'], 'header'=>$example['header'], 'title'=>$example['title'],
-            'meta'=>$example['meta description']
+        $i->amOnPage($example['url']);
+        $actualSeoData = array(
+            'title'=>$i->grabTextFrom('//title'),
+            'metaDescription'=>$i->grabAttributeFrom('//meta[@name=\'description\']', 'content')
         );
-        $actual = array(
-            'url'=>'/myPage.php', 'header'=>'some header text', 'title'=>'some title',
-            'meta description'=>'some meta description'
-        );
-        $i->assertEqualsCanonicalizing($actual, $expected);
+        $expected = array('title'=>$example['title'], 'metaDescription'=>$example['metaDescription']);
+        $i->assertEqualsCanonicalizing($expected, $actualSeoData, $example['url']);
     }
 
-    public function associativeProvider() {
-        $rows   = array_map('str_getcsv', file('./tests/resources/seo/seoDataSet.csv'));
+    private function associativeProvider() {
+        $rows = array_map('str_getcsv', file('./tests/resources/seo/seoDataSet.csv'));
         $header = array_shift($rows);
-        $csv    = array();
+        $csv = array();
         foreach($rows as $row) {
             $csv[] = array_combine($header, $row);
         }
